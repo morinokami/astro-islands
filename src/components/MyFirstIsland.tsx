1a940b048687fc18b6d2c4d3ee062a2cca0aec9f
Shinya Fujino <shf0811@gmail.com>
2023-01-15 23:49:42 +0900

    Add islands

---
+++ src/components/MyFirstIsland.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function MyFirstIsland() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
---
+++ src/components/MySecondIsland.tsx
@@ -0,0 +1,5 @@
+export function MySecondIsland() {
+  return (
+    <div>Lorem Ipsum</div>
+  );
+}
